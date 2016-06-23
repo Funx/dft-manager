476cce2d3524bdd85cb805c47601b81486c45d0b
Géraud <geraud.henrion@gmail.com>
2016-06-24 00:27:29 +0200

    structure the logger view a bit

--- src/components/Logger/index.js
+++ src/components/Logger/index.js
@@ -1,12 +1,23 @@
 import {Observable as O} from 'rx'
-import {div} from '@cycle/dom'
+import {div, ul, li, textarea} from '@cycle/dom'
 
 import css from './logger.css'
 import dot from 'utils/dot'
 
 export function Logger ({}) {
   return {
-    DOM: O.of(div(dot(css.logger), ['Hellooo'])),
+    DOM: O.of(
+      div(dot(css.logger), [
+        ul(dot(css.logs), [
+          li(dot(css.log), ['Hellooo']),
+          li(dot(css.log), ['From']),
+          li(dot(css.log), ['the']),
+          li(dot(css.log), ['other']),
+          li(dot(css.log), ['siiiiiide']),
+        ]),
+        textarea(dot(css.input), {placeholder: '>_'}),
+      ])
+    ),
     M: O.empty(),
   }
 }

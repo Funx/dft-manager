f59e34ef7c05bcc233fcc82a0624e150631d9ca1
Fuunnx <geraud.henrion@gmail.com>
2016-06-29 13:45:16 +0200

    update launch server script in package.json

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const server = express()
 
 // proxy the request for static assets
 server.get('/db', express.static(`./static/bdd.json`))
-server.post('/test', (req, res) => {
-  console.log('post')
-  res.send('POST request to the homepage')
+server.post('/db', function (req, res) {
+  res.send('I received your request buddy')
 })
+
 
 // run the two servers
 server.listen(3001, () => console.log('listening to localhost:3001'))
